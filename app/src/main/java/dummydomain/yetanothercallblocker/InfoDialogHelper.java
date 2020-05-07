c130c7853fd470f98ad1aeb2866d2bacb6307b57
xynngh <xynngh@protonmail.com>
2020-05-07 23:33:31 +0400

    Add confirmation before loading reviews for contacts

--- app/src/main/java/dummydomain/yetanothercallblocker/InfoDialogHelper.java
+++ app/src/main/java/dummydomain/yetanothercallblocker/InfoDialogHelper.java
@@ -52,16 +52,37 @@ public class InfoDialogHelper {
         ReviewsSummaryHelper.populateSummary(view.findViewById(R.id.reviews_summary),
                 numberInfo.communityDatabaseItem);
 
-        builder.setView(view);
-
-        builder.setNeutralButton(R.string.online_reviews, (d, w)
-                -> ReviewsActivity.startForNumber(context, numberInfo.number));
-
-        builder.setNegativeButton(R.string.back, null);
+        builder.setView(view)
+                .setNeutralButton(R.string.online_reviews, null)
+                .setNegativeButton(R.string.back, null);
 
         if (onDismissListener != null) builder.setOnDismissListener(onDismissListener);
 
-        builder.show();
+        AlertDialog dialog = builder.create();
+
+        // avoid dismissing the original dialog on button press
+
+        Runnable action = () -> {
+            ReviewsActivity.startForNumber(context, numberInfo.number);
+            dialog.dismiss();
+        };
+
+        dialog.setOnShowListener(x -> {
+            dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener(v -> {
+                if (numberInfo.contactItem != null) {
+                    new AlertDialog.Builder(context)
+                            .setTitle(R.string.load_reviews_confirmation_title)
+                            .setMessage(R.string.load_reviews_confirmation_message)
+                            .setPositiveButton(android.R.string.yes, (d1, w) -> action.run())
+                            .setNegativeButton(android.R.string.no, null)
+                            .show();
+                } else {
+                    action.run();
+                }
+            });
+        });
+
+        dialog.show();
     }
 
 }
